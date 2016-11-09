104c60eb20795531579c865f193b3855a47b8248
tmccarten <thomasmccarten@hotmail.com>
2016-11-09 15:27:44 +0000

    added merge media queries to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
+var mmq = require('gulp-merge-media-queries');
 
 gulp.task('sass', function() {
   return gulp.src('assets/css/**/*.scss') // Gets all files ending with .scss in assets/scss
@@ -17,10 +18,10 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', ['browserSync', 'sass'],  function(){
-  gulp.watch('assets/css/**/*.scss', ['sass']);
+  gulp.watch('assets/css/**/*.scss', ['sass', 'mmq']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('./*.html', browserSync.reload);
-  gulp.watch('assets/js/**/*.js', browserSync.reload);
+  //gulp.watch('assets/js/**/*.js', browserSync.reload);
 })
 
 gulp.task('browserSync', function() {
@@ -29,4 +30,12 @@ gulp.task('browserSync', function() {
       baseDir: ''
     },
   })
-})
+});
+
+gulp.task('mmq', function () {
+  gulp.src('assets/css/*.css')
+    .pipe(mmq({
+      log: true
+    }))
+    .pipe(gulp.dest('assets/css/'));
+});
