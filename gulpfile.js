7febe2cab39b4c6c1d51f60550cee81cc78b1e18
tmccarten <thomasmccarten@hotmail.com>
2016-11-10 00:03:29 +0000

    linked gulp-connect-php with browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
 var mmq = require('gulp-merge-media-queries');
+var connect = require('gulp-connect-php');
 
 gulp.task('sass', function() {
   return gulp.src('assets/css/**/*.scss') // Gets all files ending with .scss in assets/scss
@@ -17,20 +18,21 @@ gulp.task('sass', function() {
     }))
 });
 
-gulp.task('watch', ['browserSync', 'sass'],  function(){
+gulp.task('connect-sync', function() {
+  connect.server({}, function (){
+    browserSync({
+      proxy: '127.0.0.1:8000'
+    });
+  })
+});
+
+gulp.task('watch', ['connect-sync', 'sass'],  function(){
   gulp.watch('assets/css/**/*.scss', ['sass']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('./*.html', browserSync.reload);
-  //gulp.watch('assets/js/**/*.js', browserSync.reload);
+  gulp.watch('./*.php', browserSync.reload);
+  gulp.watch('assets/js/**/*.js', browserSync.reload);
 })
-
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-});
 
 gulp.task('mmq', function () {
   gulp.src('assets/css/*.css')
