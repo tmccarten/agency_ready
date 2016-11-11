a6111ce6931759b940019ff9f321f3230ad288f4
tmccarten <thomasmccarten@hotmail.com>
2016-11-11 00:42:09 +0000

    changed logo title to large

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <main class="page-wrapper">
     <div class="title">
       <div class="title-border-left"></div>
-        <h2 class="title-box medium">Logos</h2>
+        <h2 class="title-box large">Logos</h2>
       <div class="title-border-right"></div>
     </div>
 
