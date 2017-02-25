a633a2ce0131f84bd7077ce3144e3bb5afbe05ad
tmccarten <thomasmccarten@hotmail.com>
2017-02-25 00:55:29 +0000

    gulpfile faster

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,48 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 var mmq = require('gulp-merge-media-queries');
-var connect = require('gulp-connect-php');
+var phpConnect = require('gulp-connect-php');
+var autoPrefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
-  return gulp.src('assets/css/**/*.scss') // Gets all files ending with .scss in assets/scss
-    .pipe(sass())
-    .pipe(autoprefixer({
-            browsers: ['last 5 versions'],
+  return gulp.src('assets/css/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(autoPrefixer({
+            browsers: ['last 2 versions'],
             cascade: false
         }))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(gulp.dest('assets/css'));
 });
 
+
+
 gulp.task('connect-sync', function() {
-  connect.server({}, function (){
-    browserSync({
-      proxy: '127.0.0.1:8000'
+  phpConnect.server({
+    base: '.'
+  }, function (){
+    browserSync.init({
+      proxy: '127.0.0.1:8000',
+      baseDir: "."
     });
   });
 });
 
-gulp.task('watch', ['connect-sync', 'sass', 'mmq'],  function(){
-  gulp.watch('assets/css/**/*.scss', ['sass'], ['mmq']);
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('./**/*.html', browserSync.reload);
-  gulp.watch('*.php', browserSync.reload);
-  gulp.watch('/assets/includes/*.php', browserSync.reload);
-  gulp.watch('assets/js/**/*.js', browserSync.reload);
-});
-
 gulp.task('mmq', ['sass'], function () {
-  return gulp.src('assets/css/*.css')
+  gulp.src('assets/css/style.css')
     .pipe(mmq({
       log: true
     }))
-    .pipe(gulp.dest('assets/css'));
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+}));
+});
+
+gulp.task('watch', ['connect-sync', 'sass', 'mmq'], function(){
+ gulp.watch('assets/css/**/*.scss', ['sass', 'mmq']);
+ gulp.watch('*.php', browserSync.reload);
+ gulp.watch('assets/includes/*.php', browserSync.reload);
+ gulp.watch('assets/js/**/*.js', browserSync.reload);
+  // Other watchers
 });
